2641e91859ba4be4612bfe90cfe8b80e92d97b0a
irving00 <irvingdaniel.arizpe@gmail.com>
2026-01-06 18:13:14 -0600

    servicio para obtener las calificaciones de hoteles

--- src/main/java/com/servicio/calificacion/CalificacionmsApplication.java
+++ src/main/java/com/servicio/calificacion/CalificacionmsApplication.java
@@ -1,10 +1,10 @@
 package com.servicio.calificacion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class CalificacionmsApplication {
 
 	public static void main(String[] args) {
